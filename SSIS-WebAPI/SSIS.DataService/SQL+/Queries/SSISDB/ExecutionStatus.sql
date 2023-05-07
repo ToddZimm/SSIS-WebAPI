6bd8662e03b09cc5941e415ac2482a1a4dcc5119
Todd Zimmerman <toddzimm@outlook.com>
2023-05-07 09:38:09 -0400

    Progress on Executions data services.

---
+++ SSIS-WebAPI/SSIS.DataService/SQL+/Queries/SSISDB/ExecutionStatus.sql
@@ -0,0 +1,37 @@
+﻿--+SqlPlusRoutine
+    --&SelectType=MultiRow
+    --&Comment=Returns execution status values and descriptions
+    --&Author=Todd Zimmerman
+--+SqlPlusRoutine
+
+WITH val AS (
+  SELECT CAST(1 AS int) AS [Value]
+  UNION ALL
+  SELECT [Value] + 1
+  FROM val
+  WHERE [Value] < 9
+)
+SELECT [Value]
+  ,CAST(CASE [Value]
+     WHEN 1 THEN 'Created'
+     WHEN 2 THEN 'Running'
+     WHEN 3 THEN 'Cancelled'
+     WHEN 4 THEN 'Failed'
+     WHEN 5 THEN 'Pending'
+     WHEN 6 THEN 'Ended Unexpectedly'
+     WHEN 7 THEN 'Succeeded'
+     WHEN 8 THEN 'Stopping'
+     WHEN 9 THEN 'Completed'
+   END AS nvarchar(20)) AS [Name]
+  ,CAST(CASE [Value]
+     WHEN 1 THEN 'Execution created'
+     WHEN 2 THEN 'Execution running'
+     WHEN 3 THEN 'Execution cancelled'
+     WHEN 4 THEN 'Execution failed'
+     WHEN 5 THEN 'Execution pending'
+     WHEN 6 THEN 'Execution ended unexpectedly'
+     WHEN 7 THEN 'Execution succeeded'
+     WHEN 8 THEN 'Execution stopping'
+     WHEN 9 THEN 'Execution completed'
+   END AS nvarchar(50)) AS [Description]
+FROM val;
--- SSIS-WebAPI/SSIS.DataService/SQL+/Queries/SSISDB/Executions.sql
+++ SSIS-WebAPI/SSIS.DataService/SQL+/Queries/SSISDB/Executions.sql
@@ -1,6 +1,6 @@
---+SqlPlusRoutine
+﻿--+SqlPlusRoutine
     --&SelectType=MultiRow
-    --&Comment=Returns list of packages where the last execution was not successful.
+    --&Comment=Returns list of package executions in the SSIS Catalog
     --&Author=Todd Zimmerman
 --+SqlPlusRoutine
 
@@ -8,7 +8,30 @@
 
     DECLARE
 
-    --+Output
+    --+Default=1
+    --+Comment=Page number
+    @Page int = 1,
+
+    --+Range=10,100
+	--+Default=25
+	--+Comment=Number of records per page to return
+    @PageSize int = 25,
+   
+    --+Default=0
+    --+Comment=Provide a Package ID to filter results
+    @PackageId int = 0,
+
+    --+Default=0
+    --+Comment=Provide a status value to filter results
+    @Status int = 0,
+
+    --+Comment=Start date and time to filter results
+    @StartTime datetime,
+
+    --+Comment=End data and time to filter results
+    @EndTime datetime,
+
+     --+Output
     @Count int
 
 --+Parameters
@@ -20,9 +43,15 @@ SELECT ex.execution_id ExecutionId
   ,ex.package_name PackageName
   ,ex.status Status
   ,CAST(CASE ex.status
+     WHEN 1 THEN 'Created'
+     WHEN 2 THEN 'Running'
      WHEN 3 THEN 'Cancelled'
      WHEN 4 THEN 'Failed'
+     WHEN 5 THEN 'Pending'
      WHEN 6 THEN 'Ended Unexpectedly'
+     WHEN 7 THEN 'Succeeded'
+     WHEN 8 THEN 'Stopping'
+     WHEN 9 THEN 'Completed'
    END AS nvarchar(20)) StatusName
   ,(SELECT TOP(1) em.message
     FROM catalog.event_messages em
@@ -41,16 +70,16 @@ SELECT ex.execution_id ExecutionId
   ,ex.use32bitruntime Use32BitRuntime
 FROM catalog.executions ex 
 INNER JOIN (
-	SELECT folder_name, project_name, package_name, max(execution_id) last_execution_id
-	FROM catalog.executions
-	GROUP BY folder_name, project_name, package_name) lastexec on ex.execution_id = lastexec.last_execution_id
-INNER JOIN (
   SELECT f.name folder_name, pr.name project_name, p.name package_name, p.package_id
   FROM catalog.packages p
   LEFT JOIN catalog.projects pr on pr.project_id = p.project_id
   LEFT JOIN catalog.folders f on f.folder_id = pr.folder_id
 ) pkg ON pkg.folder_name = ex.folder_name AND pkg.project_name = ex.project_name AND pkg.package_name = ex.package_name
-WHERE ex.status IN(3, 4, 6);  --canceled (3), failed (4), ended unexpectedly (6)
-
+WHERE (@PackageId = 0 OR pkg.package_id = @PackageId)
+  AND (@StartTime IS NULL OR ex.start_time >= @StartTime)
+  AND (@EndTime IS NULL OR ex.start_time <= @EndTime)
+ORDER BY ex.start_time DESC, ex.execution_id DESC
+OFFSET ((@Page - 1) * @PageSize) ROWS
+FETCH NEXT @PageSize ROWS ONLY;
 
 SET @Count = @@ROWCOUNT;
