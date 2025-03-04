fd4e991dd410fba37dc0359fc77f5f865fd84208
Todd Zimmerman <toddzimm@outlook.com>
2025-03-04 07:04:19 -0500

    Enable Windows auth

--- SSIS-WebAPI/SSIS.API/Program.cs
+++ SSIS-WebAPI/SSIS.API/Program.cs
@@ -50,14 +50,14 @@ builder.Services.AddCors(options =>
                       });
 });
 
-//builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
-//   .AddNegotiate();
+builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+   .AddNegotiate();
 
-//builder.Services.AddAuthorization(options =>
-//{
-//    // By default, all incoming requests will be authorized according to the default policy.
-//    options.FallbackPolicy = options.DefaultPolicy;
-//});
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to the default policy.
+    options.FallbackPolicy = options.DefaultPolicy;
+});
 
 var app = builder.Build();
 
